685bf9351183a3ab3ef771d054ef47acc41210e1
iamamar-codes <kushwahaamar104@gmail.com>
2026-03-26 21:00:56 +0530

    Class and Object in c++

--- 01_classObject.cpp
+++ 01_classObject.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 
 class Student{
+
     public:
+    //Properties
     string name = "Amar Kushwaha";
     int age = 18;
     char Grade = 'A';
 };
 
 int main() {
-    Student s1;
+    Student s1; //Create Object
 
     cout<< "Student Name: " << s1.name << endl;
     cout<< "Student Age: " << s1.age << endl;
